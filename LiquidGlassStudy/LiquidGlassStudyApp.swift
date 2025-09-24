138684b09230588156eb7af9b9bd9569e2bd2d47
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-09-24 15:38:50 +0900

    리퀴드 글래스 공부 프로젝트 파일 생성

---
+++ LiquidGlassStudy/LiquidGlassStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassStudyApp.swift
+//  LiquidGlassStudy
+//
+//  Created by Youngmin Cho on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct LiquidGlassStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
