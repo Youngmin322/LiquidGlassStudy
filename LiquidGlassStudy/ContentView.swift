a34fa3a4925cbbac41d575d91e4f3297117eb3ac
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-10-06 22:30:55 +0900

    iOS 26 Glass effect 적용

--- LiquidGlassStudy/ContentView.swift
+++ LiquidGlassStudy/ContentView.swift
@@ -5,105 +5,36 @@
 //  Created by Youngmin Cho on 9/24/25.
 //
 
-import Charts
 import SwiftUI
 
-struct HikePlotView: View {
-    var body: some View {
-        Chart3D {
-            SurfacePlot(
-                x: "x", y: "y", z: "z") { x, y in
-                    sin(x) * cos(y)
-                }
-                .foregroundStyle(Gradient(colors: [.orange, .pink]))
-        }
-        .chartXScale(domain: -3 ... 6)
-        .chartYScale(domain: -3 ... 10)
-        .chartZScale(domain: -3 ... 3)
-    }
-}
-
-struct HealthTabView: View {
-    @State private var text: String = ""
-    
-    var body: some View {
-        TabView {
-            Tab("Summary", systemImage: "heart") {
-                NavigationStack {
-                    Text("Summary")
-                }
-            }
-            Tab("Sharing", systemImage: "person.2") {
-                NavigationStack {
-                    Text("Sharing")
-                }
-            }
-            Tab(role: .search) {
-                NavigationStack {
-                    Text("search")
-                }
-            }
-        }
-        .searchable(text: $text)
-    }
-}
-
 struct ContentView: View {
-    @State private var query: String = ""
-    
     var body: some View {
-        NavigationSplitView {
-            Text("Sidebar")
-        } detail: {
-            Text("Detail")
+        ZStack {
+            Image("images")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            ToTopButton()
+            
+            Spacer()
         }
-        .searchable(
-            text: $query,
-            prompt: "Search"
-            )
-        
-//        NavigationStack {
-//            TripList()
-//                .toolbar {
-//                    ToolbarItemGroup(placement: .primaryAction) {
-//                        UpButton()
-//                        DownButton()
-//                    }
-//                    
-//                    ToolbarSpacer(.fixed, placement: .primaryAction)
-//                    
-//                    ToolbarItem(placement: .primaryAction) {
-//                        SettingsButton()
-//                    }
-//                }
-//        }
     }
 }
 
-struct TripList: View {
+struct ToTopButton: View {
     var body: some View {
-        Text("Trip List")
+        Button("To Top", systemImage: "chevron.up") {
+            scrollToTop()
+        }
+        .padding()
+        .glassEffect()
     }
-}
 
-struct UpButton: View {
-    var body: some View {
-        Button("Up", systemImage: "chevron.up") { }
-    }
-}
-
-struct DownButton: View {
-    var body: some View {
-        Button("Down", systemImage: "chevron.down") { }
-    }
-}
-
-struct SettingsButton: View {
-    var body: some View {
-        Button("List Settings", systemImage: "ellipsis") { }
+    func scrollToTop() {
     }
 }
 
 #Preview {
-    HikePlotView()
+    ContentView()
 }
